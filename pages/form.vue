ea155f60aa0d6cde0c4afa4da352246f7083047b
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-08 16:36:44 -0400

    nuxt app contacts

---
+++ pages/form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form action="">
+      <UIInput label="Nome" type="text" v-model="contact.name" />
+      <UIInput label="Email" type="text" v-model="contact.email" />
+      <UIInput label="Phone" type="text" v-model="contact.phone" />
+      <UIButton type="submit" @click="saveContact">Salvar Contato</UIButton>
+    </form>
+  </div>
+</template>
+
+<script setup>
+const { createContact, getContact, updateContact } = useContacts();
+const router = useRouter();
+const route = useRoute();
+
+const contact = reactive({
+  id: "",
+  name: "",
+  email: "",
+  phone: "",
+});
+
+onMounted(() => {
+  const id = route.query.id;
+  if (id) {
+    const { name, email, phone } = getContact(id);
+
+    contact.id = id;
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+  }
+});
+
+const saveContact = () => {
+  if (contact.id) {
+    updateContact(contact.id, contact);
+  } else {
+    createContact(contact);
+  }
+
+  router.push("/");
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full px-2">
+    <ListItem v-for="contact in contacts" :key="contact.id">
+      {{ contact.name }}
+      <template #actions>
+        <button
+          class="p-1 bg-emerald-300 rounded"
+          @click="editContact(contact.id)"
+        >
+          Editar
+        </button>
+        <button
+          class="p-1 bg-red-400 text-white rounded"
+          @click="removeContact(contact.id)"
+        >
+          Excluir
+        </button>
+      </template>
+    </ListItem>
+  </div>
+</template>
+
+<script setup>
+const router = useRouter();
+const { contactList, deleteContact } = useContacts();
+const contacts = contactList();
+const editContact = (id) => {
+  router.push({ path: "/form", query: { id } });
+};
+const removeContact = (id) => {
+  deleteContact(id);
+};
+</script>
